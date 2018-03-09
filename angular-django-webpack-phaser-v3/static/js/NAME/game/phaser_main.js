b5ee27fe71a027b2dc80350e8fa14a874a466b38
pjebz <pauljasper99@gmail.com>
2018-03-09 09:44:44 -0600

    update main

--- angular-django-webpack-phaser-v3/static/js/NAME/game/phaser_main.js
+++ angular-django-webpack-phaser-v3/static/js/NAME/game/phaser_main.js
@@ -1,44 +1,8 @@
 module.exports = (function(scope, ele, attrs, window_width, window_height) {
-    var gameProperties = {
-        screenWidth: 640,
-        screenHeight: 480,
-
-        dashSize: 5,
-
-        paddleLeft_x: 50,
-        paddleRight_x: 590,
-
-        ballVelocity: 500,
-        ballRandomStartingAngleLeft: [-120, 120],
-        ballRandomStartingAngleRight: [-60, 60],
-        ballStartDelay: 2,
-    };
-
-
-    var graphicAssets = {
-        ballURL: assets_url + 'images/ball.png',
-        ballName: 'ball',
-
-        paddleURL: assets_url + 'images/paddle.png',
-        paddleName: 'paddle'
-    };
-
-    var soundAssets = {
-        ballBounceURL: assets_url + 'sound/ballBounce',
-        ballBounceName: 'ballBounce',
-
-        ballHitURL: assets_url + 'sound/ballHit',
-        ballHitName: 'ballHit',
-
-        ballMissedURL: assets_url + 'sound/ballMissed',
-        ballMissedName: 'ballMissed',
-
-        mp4URL: '.m4a',
-        oggURL: '.ogg'
-    };
-
 
     var StateMain = require('./states/StateMain.js');
+    var StateTitle = require('./states/StateTitle.js');
+
 
     console.log(window_width);
     // desktop+laptop
@@ -49,9 +13,13 @@ module.exports = (function(scope, ele, attrs, window_width, window_height) {
         // mobile
         var game = new Phaser.Game(window_width, window_height, Phaser.AUTO, 'game-canvas');
     }
-    
     game.scope = scope;
-    game.state.add('main', StateMain);
+    game.window_width = window_width;
+
+    game.state.add('StateMain', StateMain);
+    game.state.add('StateTitle', StateTitle);
+  
+
 
     // Turn off music
      scope.$on('game:toggleMusic', function() {
@@ -66,7 +34,7 @@ module.exports = (function(scope, ele, attrs, window_width, window_height) {
       // });
       game.destroy();
     });
-  
-    
-    game.state.start('main');
+
+    game.state.start('StateTitle');
+
 });
